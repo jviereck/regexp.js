978bdf25e5a8828ad2814440a54c7ada2899403d
Julian Viereck <julian.viereck@gmail.com>
2013-02-19 14:20:13 +0100

    Create node library and use browserify to get browser.js to run in the browser

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ regexp.js
 =========
 
 A JavaScript implementation of RegExp for debugging purpose.
+
+
+# Build the browser.js file under `build/`
+
+Run 
+
+```
+$> browserify index.js -d -w -o build/browser.js
+```
---
+++ index.js
@@ -0,0 +1,37 @@
+// Whole-script strict mode syntax
+"use strict";
+
+var getStartNodeFromPattern = require('./lib/exec').getStartNodeFromPattern;
+var exec = require('./lib/exec').exec;
+
+function RegExpJS(pattern, flags) {
+    if (flags !== undefined) {
+        throw new Error('Flags are not supported yet');
+    }
+
+    this.$startNode = getStartNodeFromPattern(pattern);
+    this.lastIndex = 0;
+}
+
+RegExpJS.prototype.execDebug = function RegExpJSExec(str) {
+    return exec(str, this.$startNode, this.lastIndex);
+};
+
+RegExpJS.prototype.exec = function RegExpJSExec(str) {
+    var res = this.execDebug(str);
+
+    if (res.matches) {
+        return res.matches;
+    } else {
+        return null;
+    }
+};
+
+console.log('booting!');
+
+
+if (typeof window !== undefined) {
+    window.RegExpJS = RegExpJS;
+}
+
+exports.RegExpJS = RegExpJS;
